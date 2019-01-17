dfb6517f802033ff12137a5515e7100582538b39
Miguelerja <miguelerja@gmail.com>
2019-01-17 22:10:25 +0100

    Minor changes in main-js estructure

--- JS/main.js
+++ JS/main.js
@@ -2,28 +2,7 @@
 
 var instructions = document.getElementById('instructions');
 var player2 = document.getElementById('player2');
-
 var result = document.getElementById('result');
-
-var splashScreen = `<h1 class="game-title">Gold Rush</h1>
-  <div class="buttons-container">
-  <a id="player1" class="button" href="#">I'm a solo miner</a>
-  <a id="instructions" class="button" href="#">Wanna know how to be rich?</a>
-  <a id="player2" class="button" href="#">We work in pairs</a>
-  </div>`;
-var gameScreen = `<div class="buttons-container">
-  <a class="button" href="#">10 points</a>
-  <a class="button" href="#">02:00</a>
-  <a class="button" href="#">10 points</a>
-  </div>`;
-var gameOverScreen = `<div class="final-screen">
-  <div class="header">
-  <h1 class="game-over">Game Over</h1>
-  <h2 id="result" class="result">Player 1 Won with 150 Points!</h2>
-  <a id="replay" class="replay">Insert Coin</a>
-  </div>
-  <div class="ranking"></div>
-  </div>`;
   
 function buildDom(domHtml) {
   var container = document.getElementById('container');
@@ -41,15 +20,27 @@ function startGame() {
 }
 
 function buildSplashScreen() {
+  var splashScreen = `<h1 class="game-title">Gold Rush</h1>
+  <div class="buttons-container">
+  <a id="player1" class="button" href="#">I'm a solo miner</a>
+  <a id="instructions" class="button" href="#">Wanna know how to be rich?</a>
+  <a id="player2" class="button" href="#">We work in pairs</a>
+  </div>`;
+
   buildDom(splashScreen);
   
-  var player1 = document.getElementById('player1');
   player1.addEventListener("click", function() {
     buildGameScreen();
   });
 }
 
 function buildGameScreen() {
+  var gameScreen = `<div class="buttons-container">
+  <a class="button" href="#">10 points</a>
+  <a class="button" href="#">02:00</a>
+  <a class="button" href="#">10 points</a>
+  </div>`;
+
   buildDom(gameScreen);
   
   // For testing only
@@ -66,6 +57,15 @@ function buildGameScreen() {
 }
 
 function buildGameOverScreen() {
+  var gameOverScreen = `<div class="final-screen">
+  <div class="header">
+  <h1 class="game-over">Game Over</h1>
+  <h2 id="result" class="result">Player 1 Won with 150 Points!</h2>
+  <a id="replay" class="replay">Insert Coin</a>
+  </div>
+  <div class="ranking"></div>
+  </div>`;
+  
   buildDom(gameOverScreen);
 
   var replay = document.getElementById('replay');
@@ -85,7 +85,6 @@ function destroyGameScreen(gameScreen) {
 function destroyGameOverScreen(gameOverScreen) {
   destroyDom(gameOverScreen);
 }
-
 
 
 window.addEventListener('load', startGame);
